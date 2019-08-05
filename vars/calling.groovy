9cc338ca197e654de91bd82b05a9d7d525fca9ac
Kaleeswari-G <48955447+Kaleeswari-G@users.noreply.github.com>
2019-08-05 14:51:39 +0530

    Update calling.groovy

--- vars/calling.groovy
+++ vars/calling.groovy
@@ -2,5 +2,15 @@ def call(buser){
   /*def buser=wrap([$class: 'BuildUser']) {
                     return env.BUILD_USER
                     }*/
+  def gitcommit=''
   echo "Inside Library function: ${buser}"
+  if(env.GIT_URL.contains("https://"))
+  {
+   gitcommit=env.GIT_URL.replace('.git','/')+"commit/"+env.GIT_COMMIT   
+  }
+  else
+  {
+   gitcommit="https://"+env.GIT_URL.split('@')[1].replace(':','/').replace('.git','/')+"commit/"+env.GIT_COMMIT  
+  }
+  return gitcommit
 }
